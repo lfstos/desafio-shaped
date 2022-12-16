163139f5213b8306ed10b5b3b07a21e401f6f03d
Luiz Fernando <luiz-fernando@outlook.com>
2022-12-16 18:04:20 -0300

    Criado Viewset

--- exame/views.py
+++ exame/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Exame
+from .serializers import ExameSerializer
 
 # Create your views here.
+
+class ExameViewSet(viewsets.ModelViewSet):
+    queryset = Exame.objects.all()
+    serializer_class = ExameSerializer
