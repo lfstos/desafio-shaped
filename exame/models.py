eb2277244dc10cf603cde524cc97937c8f2f3581
Luiz Fernando <luiz-fernando@outlook.com>
2022-12-16 17:44:03 -0300

    Criado modelo Exame

--- exame/models.py
+++ exame/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from paciente.models import Paciente
 
 # Create your models here.
+
+
+class Exame(models.Model):
+    profissional = models.CharField(max_length=50)
+    dt_exame = models.DateField()
+    peso = models.FloatField()
+    altura = models.FloatField()
+    
+    paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
